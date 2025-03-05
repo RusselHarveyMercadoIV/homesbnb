79496ddff5893eecaac61a637b10afbfed337198
RusselHarveyMercadoIV <russelmercadosacs@gmail.com>
2025-03-05 21:44:01 +0800

    re-add the components folder

---
+++ src/components/navigation.tsx
@@ -0,0 +1,18 @@
+import NavigationMenu from "./ui/navigationMenu";
+import { NavLink } from "react-router";
+
+export default function Navigation() {
+  return (
+    <header className="sticky top-0 bg-white flex flex-row h-30 justify-between items-center mx-20 z-100">
+      <div>
+        <h1 className="text-3xl font-bold">homesbnb</h1>
+      </div>
+      <NavigationMenu>
+        <NavLink to="/" end>
+          Explore
+        </NavLink>
+      </NavigationMenu>
+      <div className="w-[120px]"></div>
+    </header>
+  );
+}
---
+++ src/components/trending.tsx
@@ -0,0 +1,41 @@
+import { Card, CardContent } from "../components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export default function Trending() {
+  return (
+    <div
+      id="trending-section"
+      className="flex flex-col justify-center items-center gap-10 mt-20 w-full px-20"
+    >
+      {/* <h2 className="text-5xl font-bold text-slate-800"> Trending</h2> */}
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full h-fit my-20 "
+      >
+        <CarouselContent className="h-[550px]">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <div className="p-1 w-[500px] h-[300px]">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-3xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
