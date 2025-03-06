187c7fbaca8de13d781dd835f912d6a36c11d2a3
RusselHarveyMercadoIV <russelmercadosacs@gmail.com>
2025-03-06 17:03:24 +0800

    integrate google auth

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,8 @@
 import NavigationMenu from "./ui/navigationMenu";
 import { NavLink } from "react-router";
 
+import PROFILE from "../assets/icons/profile.png";
+
 export default function Navigation() {
   return (
     <header className="sticky top-0 bg-white flex flex-row h-30 justify-between items-center mx-20 z-100">
@@ -12,7 +14,16 @@ export default function Navigation() {
           Explore
         </NavLink>
       </NavigationMenu>
-      <div className="w-[120px]"></div>
+      <div className="w-[120px] flex gap-2 justify-center items-center">
+        <img src={PROFILE} alt="profile" width={35} height={35} />
+        <NavLink
+          to="/login"
+          className="bg-black text-white px-6 py-1 rounded-xl"
+        >
+          {" "}
+          Login
+        </NavLink>
+      </div>
     </header>
   );
 }
